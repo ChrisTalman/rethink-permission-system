b5525a07b751cd5ae29453cfe664201b6b2a9c12
Chris Talman <cewe.mail@gmail.com>
2020-05-27 14:35:53 +0100

    Fixed generic.

--- index.d.ts
+++ index.d.ts
@@ -55,7 +55,7 @@ declare module '@chris-talman/rethink-permission-system'
 		public readonly indexes: Indexes;
 		public readonly queries: Queries <GenericUser, GenericTargetEntityType>;
 		public isUserAuthorisedByRange({domainId, userId, permissions}: {domainId: string, userId: string, permissions: Array<GenericPermissionType>}): Promise<boolean>;
-		public isUserAuthorisedBySubject({domainId, userId, permission, subject}: {domainId: string, userId: string, permission: GenericPermissionType, subject: PermissionTargetEntity <GenericTargetEntityType>}): Promise<boolean>;
-		constructor({table, indexes, queries}: {table: string, indexes: Indexes, queries: Queries <GenericUser, GenericSubjectTargetEntityType>});
+		public isUserAuthorisedBySubject({domainId, userId, permission, subject}: {domainId: string, userId: string, permission: GenericPermissionType, subject: PermissionTargetEntity <GenericSubjectTargetEntityType>}): Promise<boolean>;
+		constructor({table, indexes, queries}: {table: string, indexes: Indexes, queries: Queries <GenericUser, GenericTargetEntityType>});
 	}
 }
